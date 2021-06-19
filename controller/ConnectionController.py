86a28cf9a64d86b5c190ba77b895fe07c195848c
Liam Flaherty <liamjfla@gmail.com>
2021-06-20 02:46:52 +0930

    add view/controller logic for connect status

--- controller/ConnectionController.py
+++ controller/ConnectionController.py
@@ -8,18 +8,20 @@ class ConnectionController:
         self.connectionModel = connectionModel
 
         self.connectionModel.devices.onValueChanged(self.onDevicesChanged)
+        self.connectionModel.connected.onValueChanged(self.onConnectedChanged)
 
     # ********************************** Helper methods *************************************
 
     # ********************************** UI event handlers **********************************
     def onConnectPressed(self, button):
-        # Make the model open the connection
-        selectedDevice = self.windowBuilder.get_object("deviceListComboBox").get_active()
-        selectedSerial = self.windowBuilder.get_object("serialPortComboBox").get_active()
-        self.connectionModel.perform_connect(selectedDevice, selectedSerial)
+        if not self.connectionModel.connected.get():
+            # Make the model open the connection
+            selectedDevice = self.windowBuilder.get_object("deviceListComboBox").get_active()
+            selectedSerial = self.windowBuilder.get_object("serialPortComboBox").get_active()
+            self.connectionModel.perform_connect(selectedDevice, selectedSerial)
 
-        # Enable rest of UI
-        self.windowBuilder.get_object("mainNotebook").set_sensitive(True)
+            # Enable rest of UI
+            self.windowBuilder.get_object("mainNotebook").set_sensitive(True)
 
     # ********************************** Model event handlers *******************************
     def onDevicesChanged(self, devices):
@@ -29,3 +31,11 @@ class ConnectionController:
             dataStore.append([device])
         
         self.windowBuilder.get_object("deviceListComboBox").set_active(0)
+
+    def onConnectedChanged(self, connected):
+        if connected:
+            self.windowBuilder.get_object("connectStatusLabel").set_label('Connected')
+            self.windowBuilder.get_object("connectButton").set_sensitive(False)
+        else:
+            self.windowBuilder.get_object("connectStatusLabel").set_label('Not connected')
+            self.windowBuilder.get_object("connectButton").set_sensitive(True)
