f72eb7eb8f2cd618fd6eba1ee192d437a7e4747f
Liam Flaherty <liamjfla@gmail.com>
2022-07-10 18:47:46 +1000

    improved log message update UI behaviour

--- controller/LogController.py
+++ controller/LogController.py
@@ -11,19 +11,40 @@ class LogController:
 
         self.logText = ''
         self.logFilter = ''
+        self.updatePaused = False
+
+        # Create mark for scrolling
+        text_buffer = self.windowBuilder.get_object('logTextBuffer')
+        text_iter_end = text_buffer.get_end_iter()
+        self.text_mark_end = text_buffer.create_mark('', text_iter_end, False)
 
         self.logModel.text.onValueChanged(self.onTextChanged)
+        self.logModel.text.onAppend(self.onTextAppend)
 
     # ********************************** Helper methods *************************************
-    def update_visible_text(self):
+    def filter_text(self, text_to_filter):
         filtered_text = ''
         if self.logFilter:
-            for line in iter(self.logText.splitlines()):
+            for line in iter(text_to_filter.splitlines()):
                 if re.search(self.logFilter, line, re.IGNORECASE):
                     filtered_text += line + '\n'
         else:
-            filtered_text = self.logText
-        self.windowBuilder.get_object("logTextBuffer").set_text(filtered_text)
+            filtered_text = text_to_filter
+        return filtered_text
+
+    def reset_text(self):
+        filtered_text = self.filter_text(self.logText)
+        text_buffer = self.windowBuilder.get_object('logTextBuffer')
+        text_buffer.set_text(filtered_text)
+
+    def append_text(self, new_text):
+        filtered_text = self.filter_text(new_text)
+        text_buffer = self.windowBuilder.get_object('logTextBuffer')
+        text_iter_end = text_buffer.get_end_iter()
+        text_buffer.insert(text_iter_end, filtered_text)
+
+    def scroll_to_end(self):
+        self.windowBuilder.get_object('logText').scroll_to_mark(self.text_mark_end, 0, False, 0, 0)
 
     # ********************************** UI event handlers **********************************
     def onLogWrapToggled(self, button):
@@ -33,13 +54,24 @@ class LogController:
             textBox.set_wrap_mode(Gtk.WrapMode.WORD)
         else:
             textBox.set_wrap_mode(Gtk.WrapMode.NONE)
+
+    def onLogPauseToggle(self, button):
+        self.updatePaused = button.get_active()
+        if self.updatePaused is False:
+            # Data will need a full refresh
+            self.reset_text()
+            self.scroll_to_end()
         
     def onLogFilterUpdated(self, search_entry):
         self.logFilter = search_entry.get_text()
-        self.update_visible_text()
+        self.reset_text()
 
 
     # ********************************** Model event handlers *******************************
     def onTextChanged(self, text):
         self.logText = text
-        self.update_visible_text()
+
+    def onTextAppend(self, full_text, new_text):
+        if not self.updatePaused:
+            self.append_text(new_text)
+            self.scroll_to_end()
--- model/ObservableField.py
+++ model/ObservableField.py
@@ -1,26 +1,35 @@
 class ObservableField:
     def __init__(self, initData = None):
-        self.notifyMethods = []
+        self.notifyValueChangedMethods = []
+        self.notifyAppendMethods = []
         self.value = initData
 
     def onValueChanged(self, method):
-        self.notifyMethods.append(method)
-    
+        self.notifyValueChangedMethods.append(method)
+
+    def onAppend(self, method):
+        self.notifyAppendMethods.append(method)
+
     def notifyValueChanged(self):
-        for notifyMethod in self.notifyMethods:
+        for notifyMethod in self.notifyValueChangedMethods:
             notifyMethod(self.value)
+
+    def notifyAppend(self, addedData):
+        for notifyMethod in self.notifyAppendMethods:
+            notifyMethod(self.value, addedData)
 
     def set(self, newValue):
         self.value = newValue
         self.notifyValueChanged()
-    
+
     def get(self):
         return self.value
-    
+
     """
     Append data of the target datatype to the interal data.
     """
     def __iadd__(self, other):
         self.value += other
         self.notifyValueChanged()
+        self.notifyAppend(other)
         return self    
